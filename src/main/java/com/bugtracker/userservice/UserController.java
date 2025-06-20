08a29861df30004b7b906b7068e33c3c6f34ae60
Rafal <rafal.z.malucha@gmail.com>
2025-06-20 02:44:51 +0200

    setup for betweem microservices communication

--- src/main/java/com/bugtracker/userservice/UserController.java
+++ src/main/java/com/bugtracker/userservice/UserController.java
@@ -2,12 +2,21 @@ package com.bugtracker.userservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class UserController {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/test")
     public String test() {
         return "This is a test - user-service";
+    }
+
+    @GetMapping("/users/projects-status")
+    public String projectStatus() {
+        String response = restTemplate.getForObject("http://project-service:8082/test", String.class);
+        return "Project Service says: " + response;
     }
 }
