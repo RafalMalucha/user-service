8bb2e0a00accc0ccde1c5883f4ee1111c53b17e8
Rafal <rafal.z.malucha@gmail.com>
2025-06-22 00:45:57 +0200

    http post setup

--- src/main/java/com/bugtracker/userservice/UserController.java
+++ src/main/java/com/bugtracker/userservice/UserController.java
@@ -1,6 +1,8 @@
 package com.bugtracker.userservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,14 +11,24 @@ public class UserController {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    public static class Info {
+        public String name;
+        public int age;
+    }
+
     @GetMapping("/test")
     public String test() {
-        return "This is a test - user-service";
+        return "This is a test - user-service" + "\n\n";
     }
 
     @GetMapping("/users/projects-status")
     public String projectStatus() {
         String response = restTemplate.getForObject("http://project-service:8082/test", String.class);
-        return "Project Service says: " + response;
+        return "Project Service says: " + response + "\n\n";
+    }
+
+    @PostMapping("/info")
+    public String receiveInfo(@RequestBody Info info) {
+        return "Hello, " + info.name + ", age " + info.age + "\n\n";
     }
 }
